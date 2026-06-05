55f3eff93654e895e16b8f1b470b2ae254d0ff0c
emdeo <ehdgml5032@gmail.com>
2026-06-05 14:25:58 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-
 const CONFIG = {
   KAKAO_MAP_KEY: "99233cbae897dc47081a3169a0a3a926",
-  LIGHTHOUSE_API_KEY: "" // 나중에 Cloudflare Workers로 처리
+  LIGHTHOUSE_API_KEY: "",
+  WEATHER_API_URL: "https://lighthouse-weather.ehdgml5032.workers.dev"
 };
