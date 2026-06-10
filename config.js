2eefea70aefb528a57508858552dc1f7ecafaeb7
emdeo <ehdgml5032@gmail.com>
2026-06-10 11:20:02 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
   KAKAO_MAP_KEY: "99233cbae897dc47081a3169a0a3a926",
   LIGHTHOUSE_API_KEY: "",
-  WEATHER_API_URL: "https://lighthouse-weather.ehdgml5032.workers.dev"
+  WEATHER_API_URL: "https://lighthouse-weather.ehdgml5032.workers.dev",
+  KAKAO_REST_KEY: "1a4c0bf9310388e325d13148c93fe2ca" 
 };
